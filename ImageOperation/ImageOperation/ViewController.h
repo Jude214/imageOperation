1f503164b488ca555c7b8a05550d98d34b101d2c
轻鸿 <qinghong@icongtai.com>
2016-05-23 17:30:40 +0800

    create project

---
+++ ImageOperation/ImageOperation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageOperation
+//
+//  Created by 廖祖德 on 16/5/23.
+//  Copyright © 2016年 QingHong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ImageOperation/ImageOperation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ImageOperation
+//
+//  Created by 廖祖德 on 16/5/23.
+//  Copyright © 2016年 QingHong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
